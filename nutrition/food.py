964057559cc27713a5d287f50e83ed428b99803a
Leo Xuzhang Lin <xuzhang@umass.edu>
2015-05-03 02:42:07 -0400

    Created a food object that stores the name and nutritional value of a food article

---
+++ nutrition/food.py
@@ -0,0 +1,19 @@
+# An article of food available for consumption
+class Food:
+
+	# Contains:
+	# Name/identifier of the food
+	# Array of tuple value for the nutrition
+	def __init__(self, name, nutrition=[]):
+		self.name=name;
+		self.nutrition=nutrition;
+
+	# Append a nutritional value to the array of nutritions
+	# Nutrition is a tuple:
+	# (name of the nutrition[string], % daily value[double])
+	def add_nutrition(self, nutrition):
+		self.nutrition.append(nutrition);
+
+	# Return the nutritional value of a food product
+	def get_nutrition(self):
+		return self.nutrition
